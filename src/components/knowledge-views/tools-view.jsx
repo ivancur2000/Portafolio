6ed215cd34f31b4ec89616391d0d435cb294f097
Ivan Curtiñez <ivancurtinez.charli@gmail.com>
2021-03-04 14:33:02 -0400

    No se que estoy haciendo XD

--- src/components/knowledge-views/tools-view.jsx
+++ src/components/knowledge-views/tools-view.jsx
@@ -1,8 +1,20 @@
-import {React} from 'react';
+import {React, Fragment} from 'react';
+import Card from 'react-bootstrap/Card';
 
 const ToolsView = ()=>{
   return(
-    <h1>las herramientas que domino</h1>
+    <Fragment>
+      <Card>
+        <Card.Header>
+          <h1>Mis Skills</h1>
+        </Card.Header>
+        <Card.Body>
+          <h2>Programacion Frontend</h2>
+          <h2>Programacion Backend</h2>
+          <h2>Otras Herramientas</h2>
+        </Card.Body>
+      </Card>
+    </Fragment>
   );
 }
 
